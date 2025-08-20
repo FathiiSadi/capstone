74060099316b654b0a98a27754f8ea7b7500c6b9
SolveX <fathii.alsadi@gmail.com>
2025-08-20 12:50:21 +0300

    fix migrations

--- database/migrations/2025_08_20_094252_create_instructor_table.php
+++ database/migrations/2025_08_20_094252_create_instructor_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// Removed unused directive
-
 return new class extends Migration
 {
     /**
@@ -15,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('instructor', function (Blueprint $table) {
             $table->id();
-
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('department_id')->constrained('department');
-
-            $table->integer('min_credit_hours');
             $table->integer('active_credit_hours');
+            $table->timestamps();
         });
     }
 
